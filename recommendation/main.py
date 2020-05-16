f7348648bfba909940d9b4e8b01c8f8b086dc588
masaa96 <mashadjurisich@gmail.com>
2020-05-16 14:57:17 +0200

    petljanje sa bazom, nasla sam neki SQLAlchemy

--- recommendation/main.py
+++ recommendation/main.py
@@ -1,9 +1,24 @@
 from flask import Flask, url_for, render_template, request, redirect, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 @app.route('/')
@@ -44,4 +59,5 @@ def logout():
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True, threaded=True)
