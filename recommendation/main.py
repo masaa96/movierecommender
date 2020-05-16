ba21be4902a1d65a104743529b7e5cb63ced8e1b
masaa96 <mashadjurisich@gmail.com>
2020-05-16 13:27:30 +0200

    metode GET i POST, session..

--- recommendation/main.py
+++ recommendation/main.py
@@ -1,11 +1,42 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, redirect, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 @app.route('/')
 def gallery():
     return render_template("gallery.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("gallery.html")
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
